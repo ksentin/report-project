121ff5e39de6e3399978c9cf9f61f0ade7dfd93d
ksentin <ksenpati@gmail.com>
2024-03-02 14:52:53 +0200

    add lab1 and lab2 html-files

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 function changePage(pageName) {
     var mainContent = document.getElementById('mainContent');
 
-    fetch(pageName)
+    var labsFolderPath = 'labs/';
+    
+    var labFilePath = labsFolderPath + pageName;
+
+    fetch(labFilePath)
         .then(response => response.text())
         .then(data => {
             mainContent.innerHTML = data;
         })
         .catch(error => console.error('Error:', error));
 }
+
